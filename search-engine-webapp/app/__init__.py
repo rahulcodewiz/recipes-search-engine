91aa32ba14e708ab22d6af8f25866b2489cdcd3b
Rahul Sharma <reethified@gmail.com>
2020-11-01 10:05:24 -0800

    add controller

--- search-engine-webapp/app/__init__.py
+++ search-engine-webapp/app/__init__.py
@@ -1,10 +1,37 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
 import os
 from flask import Flask, render_template
+from json2html import *
+import requests
+import json
+import os
+from elasticsearch import Elasticsearch
+
 app = Flask(__name__)
 app.config.from_pyfile('setup.cfg', silent=True)
+
+es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
 @app.route('/')
 def home():
    return render_template('index.html')
+
+
+@app.route('/search' , methods=['POST'])
+def search():
+   return searchEs(request.form['recipesText'])
+
+
 if __name__ == '__main__':
    app.run()
+
+
+def searchEs(term):
+   query = json.dumps({
+        "query": {
+            "match": {
+                "title": term
+            }
+        }
+    })
+   res = es.search(index="recipes_idx1", body=query)
+   return json2html.convert(json = res)
