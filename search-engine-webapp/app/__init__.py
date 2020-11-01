a376896ed5b44716270acf390d8cfe0da8598efd
Rahul Sharma <reethified@gmail.com>
2020-11-01 10:05:08 -0800

    Add type ahead feature

--- search-engine-webapp/app/__init__.py
+++ search-engine-webapp/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, flash
 import os
 from flask import Flask, render_template
 app = Flask(__name__)
-
+app.config.from_pyfile('setup.cfg', silent=True)
 @app.route('/')
 def home():
    return render_template('index.html')
---
+++ search-engine-webapp/app/static/js/esQueries.js
@@ -0,0 +1,56 @@
+//Ready function when page is loaded
+$(document).ready(function() {
+    //Autocomplete function
+    var autoCompleteDemo = function(query, cb) {
+        var results = $.map([0], function() {
+            //Get text from the input field
+            var text = document.getElementById('typeahead').value;
+            //ES Query
+            var json = {
+                suggest: {
+                    text: text,
+                    recipes: {
+                        term: {
+                            field: "title"
+                        }
+                    }
+                }
+            };
+
+            //Ajax call to ES make sure this matches YOUR ES info
+            var request = $.ajax({
+                type: "POST",
+                url: "http://localhost:9200/recipes_idx1/_search",
+                async: false,
+                data: JSON.stringify(json),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data) {
+                    return (data);
+                },
+                failure: function(errMsg) {
+                    alert(errMsg);
+                }
+            });
+
+            //Parse the results and return them
+            var response = JSON.parse(request.responseText);
+            var resultsArray = response.suggest.recipes[0].options;
+            var datum = [];
+            for (var i = 0; i < resultsArray.length; i++) {
+                datum.push({
+                    theValue: resultsArray[i].text
+                });
+            }
+            return datum;
+        });
+
+        cb(results);
+    };
+
+    $('.typeahead').typeahead(null, {
+        displayKey: 'theValue',
+        source: autoCompleteDemo
+    });
+
+});
