27a193d9c9a90b7a3e5c04ce71dcdf20f169a8d7
Pradeep, Sakhamoori <pradeep.sakhamoori@intel.com>
2020-11-15 21:37:39 -0700

    Run elasticserach as background process

--- search-engine-webapp/start_app.sh
+++ search-engine-webapp/start_app.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+echo "Starting Elastic Server"
+cd /app/elasticsearch-7.10.0/
+cmd="./bin/elasticsearch"
+
 export FLASK_APP=app
 export FLASK_ENV=development
+$cmd > /dev/null 2>&1 &
 flask run
