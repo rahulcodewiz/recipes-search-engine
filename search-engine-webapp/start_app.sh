5cb410b106fe4b3c6010d8c891be1d228ef9d8f8
Rahul Sharma <reethified@gmail.com>
2020-11-21 15:32:51 -0800

    Revert start app as it's used in docker

--- search-engine-webapp/start_app.sh
+++ search-engine-webapp/start_app.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-echo "Check if ElasticSearch is running"
-es_status=`curl 'localhost:9200'`
-if [[ "$es_status" == *"not"* ]]; then
-    echo "Starting Elastic Server"
-    export ES_HOME="${ES_HOME:-'/app/elasticsearch-7.10.0/'}"
-    echo "Starting Elastic Search from ${ES_HOME}"
-    bash ${ES_HOME}/bin/elasticsearch > /dev/null 2>&1 &
-    if [ $? -ne 0 ]; then
-    echo "Failed to start ElasticSearch"
-    exit 1
-    fi
-else
-    echo "Elastic Search is running: $es_status"
-fi
+echo "Starting Elastic Server"
+cd /app/elasticsearch-7.10.0/
+cmd="./bin/elasticsearch"
 
-export FLASK_APP="${FLASK_APP:-app}"
-export FLASK_ENV="${FLASK_APP:-development}"
+export FLASK_APP=app
+export FLASK_ENV=development
+$cmd > /dev/null 2>&1 &
 flask run
